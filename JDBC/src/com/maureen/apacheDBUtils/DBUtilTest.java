e4a91504c740493e91ff7530e59a80753e2e7f55
Maureen <1437867946@qq.com>
2020-07-05 15:35:40 +0800

    结果集放到不同的Handler中，DBUtils中API的应用

--- JDBC/src/com/maureen/apacheDBUtils/DBUtilTest.java
+++ JDBC/src/com/maureen/apacheDBUtils/DBUtilTest.java
@@ -3,12 +3,15 @@ package com.maureen.apacheDBUtils;
 import com.maureen.entity.Emp;
 import com.maureen.util.MySqlDBUtil;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.BeanHandler;
-import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.*;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DBUtilTest {
 
@@ -29,7 +32,7 @@ public class DBUtilTest {
     }
 
     /**
-     * 查询结果集
+     * 查询结果集，将结果集放到List中
      * @throws SQLException
      */
     public static void testList() throws SQLException {
@@ -43,8 +46,109 @@ public class DBUtilTest {
         connection.close();
     }
 
+    public static void testArray() throws SQLException {
+        connection = MySqlDBUtil.getConnection();
+        String sql = "select * from emp";
+        QueryRunner runner = new QueryRunner();
+        Object[] object = runner.query(connection, sql, new ArrayHandler());
+        for(Object o: object) {
+            System.out.println(o);
+        }
+        connection.close();
+    }
+
+    public static void testArrayList() throws SQLException {
+        connection = MySqlDBUtil.getConnection();
+        String sql = "select * from emp";
+        QueryRunner runner = new QueryRunner();
+        List<Object[]> query = runner.query(connection, sql, new ArrayListHandler());
+        for(Object[] objects : query) {
+            System.out.println(objects[0]+ "--" + objects[1]);
+        }
+        connection.close();
+    }
+
+    public static void testMap() throws SQLException {
+        connection = MySqlDBUtil.getConnection();
+        String sql = "select * from emp";
+        QueryRunner runner = new QueryRunner();
+        Map<String, Object> query = runner.query(connection, sql, new MapHandler());
+        Set<Map.Entry<String,Object>> entries = query.entrySet();
+        for (Map.Entry<String, Object> entry : entries) {
+            System.out.println(entry.getKey() + "----" + entry.getValue());
+        }
+        connection.close();
+    }
+
+    public static void testScalarHandler() throws SQLException {
+        connection = MySqlDBUtil.getConnection();
+        String sql = "select count(*) from emp";
+        QueryRunner runner = new QueryRunner();
+        Object query = runner.query(connection, sql, new ScalarHandler<>());
+        System.out.println(query);
+        connection.close();
+    }
+
+    /**
+     * 自定义Handler对象
+     * @throws SQLException
+     */
+    public static void testMyHandler() throws SQLException {
+        connection = MySqlDBUtil.getConnection();
+        String sql = "select * from emp where empno = ?";
+        QueryRunner runner = new QueryRunner();
+        Emp emp = runner.query(connection, sql, new ResultSetHandler<Emp>() {
+            @Override
+            public Emp handle(ResultSet resultSet) throws SQLException {
+                if (resultSet.next()) {
+                    Emp e = new Emp();
+                    e.setEmpno(resultSet.getInt("empno"));
+                    e.setEname(resultSet.getString("ename"));
+                    return e;
+                }
+                return null;
+            }
+        }, 7369);
+        System.out.println(emp);
+        connection.close();
+    }
+
+    public static void insert() throws SQLException {
+        String sql = "insert into emp(empno,ename) values(?,?)";
+        Connection connection = MySqlDBUtil.getConnection();
+        QueryRunner queryRunner = new QueryRunner();
+        queryRunner.update(connection, sql, 1234, "maureen");
+        connection.close();
+    }
+
+    public static void update() throws SQLException {
+        String sql = "update emp set ename=? where empno = ?";
+        Connection connection = MySqlDBUtil.getConnection();
+        QueryRunner queryRunner = new QueryRunner();
+        queryRunner.update(connection, sql, "maureen1", 1234);
+        connection.close();
+    }
+
+
+    public static void delete() throws SQLException {
+        String sql = "delete from emp where empno = ?";
+        Connection connection = MySqlDBUtil.getConnection();
+        QueryRunner queryRunner = new QueryRunner();
+        queryRunner.update(connection, sql, 1234);
+        connection.close();
+    }
+
+
     public static void main(String[] args) throws SQLException {
         //testQuery();
-        testList();
+        //testList();
+        //testArray();
+        //testArrayList();
+        //testMap();
+        //testScalarHandler();
+        //testMyHandler();
+        //insert();
+        //update();
+        delete();
     }
 }
